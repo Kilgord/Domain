f0a10a1be025c2d76e92ac6eb3b4844de3f1e034
Kilgord <kostannnn41@gmail.com>
2019-10-09 23:19:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+namespace ConsoleApp13
+{
+    public class Test
+    {
+       
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string domain = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName; // Получение домена(если его нет, то выдает пустое значение)
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(@"root\CIMV2", @"SELECT * FROM Win32_ComputerSystem");
+            
+            if (domain == null)
+            {
+                Console.WriteLine("DomainName: {0}", domain);
+            }
+            else
+            {
+                foreach (ManagementObject wg in mos.Get())
+                {
+                    Console.WriteLine("Workgroup: " + wg["Workgroup"]);
+                }
+            }
+            Console.WriteLine("UserName: {0}", Environment.UserName); // Получение имени
+
+            Console.ReadKey();
+
+        }
+    }
+}
